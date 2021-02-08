f90b3b8093f370c7f13d9903e7a2a0891972efe1
MutantWafflez <61396445+MutantWafflez@users.noreply.github.com>
2021-02-07 18:53:30 -0800

    Modifier Appear UI Part 1

--- BlessingsMod.cs
+++ BlessingsMod.cs
@@ -1,20 +1,62 @@
 using BlessingsMod.Content.Modifiers;
+using BlessingsMod.Content.UI;
 using BlessingsMod.Custom;
 using BlessingsMod.Custom.Interfaces;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
 using System;
 using System.Collections.Generic;
+using Terraria;
+using Terraria.Graphics.Shaders;
+using Terraria.ID;
 using Terraria.ModLoader;
+using Terraria.UI;
 
 namespace BlessingsMod {
 
     public class BlessingsMod : Mod {
+
+        #region Modifier Lists
+
         public static List<Blessing> possibleBlessings;
 
         public static List<Curse> possibleCurses;
 
         public static List<IModifier> activeModifiers;
 
+        #endregion
+
+        #region UI Fields
+
+        internal UserInterface modifierRevealInterface;
+
+        internal ModifierRevealUI revealUIState;
+
+        internal GameTime lastGameTime;
+
+        #endregion
+
+        public static BlessingsMod ModInstance {
+            internal set;
+            get;
+        }
+
+        public BlessingsMod() {
+            ModInstance = this;
+
+            Properties = new ModProperties() {
+                Autoload = true,
+                AutoloadGores = true,
+                AutoloadSounds = true
+            };
+        }
+
+        #region Loading
+
         public override void Load() {
+
+            #region List Initialization
+
             possibleBlessings = new List<Blessing>();
             possibleCurses = new List<Curse>();
             activeModifiers = new List<IModifier>();
@@ -26,6 +68,19 @@ namespace BlessingsMod {
             foreach (Type curseType in BlessingUtilities.GetAllChildrenOfClass<Curse>()) {
                 possibleCurses.Add((Curse)Activator.CreateInstance(curseType));
             }
+
+            #endregion
+
+            #region UI Initialization
+
+            if (Main.netMode != NetmodeID.Server) {
+                modifierRevealInterface = new UserInterface();
+                revealUIState = new ModifierRevealUI();
+                revealUIState.Activate();
+                modifierRevealInterface.SetState(revealUIState);
+            }
+
+            #endregion
         }
 
         public override void Unload() {
@@ -33,5 +88,33 @@ namespace BlessingsMod {
             possibleCurses = null;
             activeModifiers = null;
         }
+
+        #endregion
+
+        #region UI
+
+        public override void UpdateUI(GameTime gameTime) {
+            lastGameTime = gameTime;
+            if (modifierRevealInterface?.CurrentState != null) {
+                modifierRevealInterface.Update(gameTime);
+            }
+        }
+
+        public override void ModifyInterfaceLayers(List<GameInterfaceLayer> layers) {
+            int mouseTextIndex = layers.FindIndex(layer => layer.Name.Equals("Vanilla: Mouse Text"));
+            if (mouseTextIndex != -1) {
+                layers.Insert(mouseTextIndex, new LegacyGameInterfaceLayer(
+                    "BlessingsMod: Modifier Reveal Panel",
+                    delegate {
+                        if (lastGameTime != null && modifierRevealInterface?.CurrentState != null) {
+                            modifierRevealInterface.Draw(Main.spriteBatch, lastGameTime);
+                        }
+                        return true;
+                    },
+                       InterfaceScaleType.UI));
+            }
+        }
+
+        #endregion
     }
 }
---
+++ Content/UI/ModifierRevealUI.cs
@@ -0,0 +1,125 @@
+﻿using BlessingsMod.Content.Modifiers;
+using BlessingsMod.Content.Modifiers.Blessings;
+using BlessingsMod.Custom.Interfaces;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using Terraria;
+using Terraria.GameContent.UI.Elements;
+using Terraria.ModLoader;
+using Terraria.UI;
+using Terraria.UI.Chat;
+
+namespace BlessingsMod.Content.UI {
+
+    public class ModifierRevealUI : UIState {
+        private const int backPanelWidth = 600;
+        private const int backPanelHeight = 400;
+        private const int backPanelPadding = 6;
+
+        //Cinematic Float Fields
+        private float backPanelHeightMultiplier;
+
+        private float backPanelWidthMultiplier;
+        private float contentPanelHeightMultiplier;
+        private float contentPanelWidthMultiplier;
+        private float cinematicStage;
+        private float cinematicTimer;
+
+        private IModifier appliedModifier;
+        private Rectangle backPanel;
+        private Rectangle contentPanel;
+
+        //Cinematic Bool Fields
+        private bool appearanceCinematicRolling;
+
+        private bool useOutlineShader;
+
+        //Misc Cinematic Fields
+        private Color targetColor;
+
+        public override void OnInitialize() {
+            TriggerAppearance(new TestBlessing());
+        }
+
+        public void TriggerAppearance(IModifier newModifer) {
+            appliedModifier = newModifer;
+            backPanelHeightMultiplier = 0.0125f;
+            backPanelWidthMultiplier = 0f;
+            contentPanelHeightMultiplier = 0f;
+            contentPanelWidthMultiplier = 0f;
+            appearanceCinematicRolling = true;
+            useOutlineShader = false;
+            cinematicStage = 0f;
+            cinematicTimer = 0f;
+            targetColor = new Color(1f, 1f, 1f, 1f);
+        }
+
+        public void RollCinematic() {
+            if (cinematicStage == 0f) {
+                backPanelWidthMultiplier = MathHelper.Clamp(backPanelWidthMultiplier + (0.0025f * (1 + backPanelWidthMultiplier * 12f)), 0f, 1f);
+                if (backPanelWidthMultiplier >= 1f) {
+                    cinematicStage = 1f;
+                    cinematicTimer = 0f;
+                }
+            }
+            else if (cinematicStage == 1f) {
+                if (++cinematicTimer > 15f) {
+                    backPanelHeightMultiplier += 0.005f * (1 + backPanelHeightMultiplier * 12f);
+                    backPanelHeightMultiplier = MathHelper.Clamp(backPanelHeightMultiplier, 0.0125f, 1f);
+                }
+                if (backPanelHeightMultiplier >= 1f) {
+                    cinematicStage = 2f;
+                    cinematicTimer = 0f;
+                    contentPanelHeightMultiplier = 0.0125f;
+                }
+            }
+            else if (cinematicStage == 2f) {
+                contentPanelWidthMultiplier = MathHelper.Clamp(contentPanelWidthMultiplier + (0.0025f * (1 + contentPanelWidthMultiplier * 12f)), 0f, 1f);
+                if (contentPanelWidthMultiplier >= 1f) {
+                    cinematicStage = 3f;
+                    cinematicTimer = 0f;
+
+                }
+            }
+            else if (cinematicStage == 3f) {
+                if (++cinematicTimer > 15f) {
+                    contentPanelHeightMultiplier += 0.005f * (1 + contentPanelHeightMultiplier * 12f);
+                    contentPanelHeightMultiplier = MathHelper.Clamp(contentPanelHeightMultiplier, 0.0125f, 1f);
+                }
+                if (contentPanelHeightMultiplier >= 1f) {
+                    cinematicStage = 4f;
+                    cinematicTimer = 0f;
+                }
+            }
+            else if (cinematicStage == 4f) {
+                if (++cinematicTimer >= 30f) {
+                    if (appliedModifier is Blessing) {
+                        targetColor = new Color(0.2f, 0.8f, 0.2f);
+                        //Happy sound
+                    }
+                    else if (appliedModifier is Curse) {
+                        targetColor = new Color(1f, 0f, 0f);
+                        //Anger sound
+                    }
+                    cinematicStage = 5f;
+                }
+            }
+            else if (cinematicStage == 5f) {
+                TriggerAppearance(new TestBlessing());
+            }
+        }
+
+        protected override void DrawSelf(SpriteBatch spriteBatch) {
+            if (appearanceCinematicRolling && !Main.gameInactive) {
+                RollCinematic();
+            }
+
+            backPanel = new Rectangle((Main.screenWidth / 2) - (backPanelWidth / 2), (Main.screenHeight / 4) - (backPanelHeight / 2), (int)(backPanelWidth * backPanelWidthMultiplier), (int)(backPanelHeight * backPanelHeightMultiplier));
+            contentPanel = new Rectangle(backPanel.X + (backPanelPadding / 2), backPanel.Y + (backPanelPadding / 2), (int)((backPanelWidth - backPanelPadding) * contentPanelWidthMultiplier), (int)((backPanelHeight - backPanelPadding) * contentPanelHeightMultiplier));
+
+            spriteBatch.Draw(Main.magicPixel, backPanel, targetColor);
+            spriteBatch.Draw(Main.magicPixel, contentPanel, new Color(0.5f, 0.5f, 0.5f));
+        }
+    }
+}
